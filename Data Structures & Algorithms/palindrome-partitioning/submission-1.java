f8a5036ff0aba1939fa045bce4c9df48a5a734ee
Deesh <145862991+Deesh-singla@users.noreply.github.com>
2026-05-27 20:25:57 +0530

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean check(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+        }
+        return true;
+    }
+    public void helper(int i, String s, List<String> list, List<List<String>> ans) {
+        if (i == s.length()) {
+            int flag = 0;
+            for (String str : list) {
+                if (check(str) == false) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+                ans.add(new ArrayList<>(list));
+            return;
+        }
+        StringBuilder x = new StringBuilder();
+        for (int j = i; j < s.length(); j++) {
+            x.append(s.charAt(j));
+            if (check(x.toString())) {
+                list.add(x.toString());
+                helper(j + 1, s, list, ans);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        helper(0, s, list, ans);
+        return ans;
+    }
+}
