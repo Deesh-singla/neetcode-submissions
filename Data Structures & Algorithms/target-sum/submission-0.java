1451755345429f47632bdecbecfcab9adf71c761
Deesh <145862991+Deesh-singla@users.noreply.github.com>
2026-05-29 04:37:50 +0530

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int helper(int i, int[] nums, int target, int total, int[][] dp, int sum) {
+        if (target == sum && i == nums.length)
+            return 1;
+        if (i >= nums.length)
+            return 0;
+        if (dp[i][sum + total] != -1)
+            return dp[i][sum + total];
+        return dp[i][sum + total] = helper(i + 1, nums, target, total, dp, sum + nums[i])
+            + helper(i + 1, nums, target, total, dp, sum - nums[i]);
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int it : nums) sum += it;
+        int dp[][] = new int[nums.length][sum * 2 + 3];
+        for (int[] it : dp) Arrays.fill(it, -1);
+        return helper(0, nums, target, sum, dp, 0);
+    }
+}
