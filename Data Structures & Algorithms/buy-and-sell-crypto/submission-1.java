cc4604dd8cf53e9b0bb5201c6e7eb8841ae42f52
Deesh <145862991+Deesh-singla@users.noreply.github.com>
2026-05-29 22:51:01 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int p = prices[0];
+        int ans = 0;
+        for (int i = 1; i < prices.length; i++) {
+            ans = Math.max(ans, prices[i] - p);
+            if (prices[i] < p)
+                p = prices[i];
+        }
+        return ans;
+    }
+}
